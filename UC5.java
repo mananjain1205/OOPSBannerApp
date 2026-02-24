6e6f44971d97a6d77a5f5bc4aadf54d9398a9706
mananjain1205 <mj4328@srmist.edu.in>
2026-02-24 10:17:42 +0530

    Upload UC5

---
+++ UC5.java
@@ -0,0 +1,11 @@
+public class UC5 {
+    public static void main(String[] args) {
+        System.out.println(String.join("\n", new String[] {
+            "  ***   ***  ****  **** ",
+            " *   * *   * *   * *  ",
+            " *   * *   * ****  *** ",
+            " *   * *   * *       * ",
+            "  ***   ***  *    **** "
+        }));
+    }
+}
